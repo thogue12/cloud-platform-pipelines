224b61397ce224e65421d0fa602e4d9b443f8f5d
thogue12 <hoguetimothy12@gmail.com>
2026-02-26 11:24:28 -0500

    another commit to the pipeline

--- jenkins/deploy-az-platform.groovy
+++ jenkins/deploy-az-platform.groovy
@@ -2,7 +2,7 @@ import groovy.json.JsonSlurper
 @Library('shared-library') _
 
 // Define the logic as Closures at the top of the file
-def getSubscriptions{'''
+def getSubscriptions{"""
 import groovy.json.JsonSlurper
     try {
         def process = ['/usr/bin/az', 'account', 'list', '--query', '[].{name:name, id:id}', '--output', 'json'].execute()
@@ -13,13 +13,13 @@ import groovy.json.JsonSlurper
             return data.collect { "${it.name} (${it.id})" }
         }
         return ["Error: CLI Failed"]
-    } catch (e) { return ["Error: ${e.message}"] }'''
+    } catch (e) { return ["Error: ${e.message}"] }"""
 }
 
 
 
 // Define this at the very top of your Jenkinsfile, before the 'properties' block
-def getStorageAccounts(selectedSub){ '''
+def getStorageAccounts(selectedSub){ """
 import groovy.json.JsonSlurper
     try {
         // 1. Check if the parent parameter is empty or null
@@ -55,7 +55,7 @@ import groovy.json.JsonSlurper
                          
     } catch (Exception e) {
         return ["GROOVY ERROR: " + e.getMessage().take(50)]
-    }'''
+    }"""
 }
 
 // Then in your properties block, you call it like this:
